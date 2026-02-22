59953e0bcc0805b42c32121409c0e3c3bb199a03
Marina Yastrebova <yasstreb@gmail.com>
2026-02-22 15:15:26 +0200

    clear commit

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,7 +1,6 @@
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
-  console.log('MONGO_URL:', process.env.MONGO_URL);
   await mongoose.connect(process.env.MONGO_URL);
   console.log('✅ MongoDB connection established successfully');
 };
