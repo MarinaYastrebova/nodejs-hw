18e9ae5b57a4a9d4f7db2aa20bce79ed3aba916f
Marina Yastrebova <yasstreb@gmail.com>
2026-02-22 14:27:56 +0200

    work commit

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -20,14 +20,14 @@ export const createNote = async (req, res) => {
 
 export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndDelete({ _id: noteId });
+  const note = await Note.findByIdAndDelete(noteId);
   if (!note) throw createHttpError(404, 'Note not found');
   res.status(200).json(note);
 };
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findByIdAndUpdate({ _id: noteId }, req.body, {
+  const note = await Note.findByIdAndUpdate(noteId, req.body, {
     new: true,
   });
   if (!note) throw createHttpError(404, 'Note not found');
