741ef65cadf039da92598aa367a8ee44b1d9aa96
Marina Yastrebova <yasstreb@gmail.com>
2026-02-28 23:40:17 +0200

    add validation, pagination, filtering

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,8 +2,29 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
-  res.status(200).json(notes);
+  const { tag, search, page = 1, perPage = 10 } = req.query;
+  const filter = {};
+  if (tag) {
+    filter.tag = tag;
+  }
+  if (search) {
+    filter.$text = { $search: search };
+  }
+
+  const skip = (page - 1) * perPage;
+
+  const totalNotes = await Note.countDocuments(filter);
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  const notes = await Note.find(filter).skip(skip).limit(perPage);
+
+  res.status(200).json({
+    page: Number(page),
+    perPage: Number(perPage),
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -6,21 +7,12 @@ const noteSchema = new Schema(
     content: { type: String, default: '', trim: true },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
   { timestamps: true },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 export const Note = model('Note', noteSchema);
