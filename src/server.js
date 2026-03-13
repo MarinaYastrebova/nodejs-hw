c1396d5fc54b143aeade4e5f704e8072b5bb83ef
Marina Yastrebova <yasstreb@gmail.com>
2026-03-13 22:53:20 +0200

    add edits 2

--- src/server.js
+++ src/server.js
@@ -22,9 +22,9 @@ const startServer = async () => {
   app.use(authRoutes);
   app.use(notesRoutes);
   app.use(userRoutes);
-  app.use(errorHandler);
   app.use(notFoundHandler);
   app.use(errors());
+  app.use(errorHandler);
 
   const PORT = process.env.PORT ?? 3000;
 
